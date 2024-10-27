d61000b69463bcf08794a6f7a942af69a30d2e8a
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-10-27 23:07:53 +0300

    Created applications User and habits

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from habits.models import Habits
 
-# Register your models here.
+
+@admin.register(Habits)
+class Habits(admin.ModelAdmin):
+    list_display = ('user', 'location', 'times', 'move', 'is_sign_of_pleasant_habit', 'related_habit', 'periodicity', 'reward', 'time_to_complete', 'is_public')
--- habits/models.py
+++ habits/models.py
@@ -5,22 +5,9 @@ from users.models import NULLABLE
 
 
 class Habits(models.Model):
-    ED = 'every day"'
-    EOD = 'every other day'
-    OETD = 'once every three days'
-    OEFD = 'once every four days'
-    OEFiD = 'once every five days'
-    OESD = 'once every six days'
-    OW = 'once a week'
-
-    PERIOD: [
-        ("ED", "каждый день"),
-        ("EOD", "через день"),
-        ("OETD", "раз в три дня"),
-        ("OEFD", "раз в четыре дня"),
-        ("OEFiD", "раз в пять дня"),
-        ("OESD", "раз в шесть дней"),
-        ("OW", "раз в неделю"),
+    PERIOD_DAY = [
+        ('EVERYDAY', 'каждый день'),
+        ('EVERYWEEK', 'раз в неделю'),
     ]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Создатель привычки")
@@ -28,14 +15,14 @@ class Habits(models.Model):
     times = models.TimeField(verbose_name="Время, когда необходимо выполнить привычку")
     move = models.CharField(max_length=250, verbose_name="Действие")
     is_sign_of_pleasant_habit = models.BooleanField(default=False, verbose_name="Признак приятной привычки")
-    related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name="Связанная привычка")
-    periodicity = models.CharField(max_length=50, default=ED, verbose_name="Периодичность", choices=PERIOD, **NULLABLE)
+    related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name="Связанная привычка", **NULLABLE)
+    periodicity = models.CharField(max_length=50, default="EVERYDAY", verbose_name="Периодичность", choices=PERIOD_DAY)
     reward = models.CharField(max_length=250, verbose_name="Вознаграждение", **NULLABLE)
-    time_to_complete = models.SmallIntegerField(verbose_name="Время на выполнение привычки", help_text="Время указать в секундах", **NULLABLE)
+    time_to_complete = models.TimeField(auto_now=False, auto_now_add=False, verbose_name="Время на выполнение привычки", **NULLABLE)
     is_public = models.BooleanField(default=False, verbose_name="Видно всем")
 
     def __str__(self):
-        return {self.move}
+        return self.move
 
     class Meta:
         verbose_name = 'Привычка'
